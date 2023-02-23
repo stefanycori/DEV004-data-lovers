0c8dd8237ecdba0915577a825f42cdef3ad881ac
unknown <stefanycori9@gmail.com>
2023-02-23 16:14:15 -0500

    cambios js, html, css

--- src/data.js
+++ src/data.js
@@ -7,3 +7,64 @@ export const example = () => {
 export const anotherExample = () => {
   return 'OMG';
 };
+import pokemon, { default as dataPokemon } from "./data/pokemon/pokemon.js";
+
+const arrayPokemon = dataPokemon.pokemon.slice(0, 100);
+export default arrayPokemon;
+
+let contenedor = document.getElementById('container');
+
+//recibe como parametro arreglo de pokemones
+export const templateCard = (data) => { //exporta la funcion en una constante
+  console.log(data)
+
+  let htmlCode = '';    //variable para almacenar codigo html
+
+  data.forEach(poke => {
+    //genera un div poor cada pokemon
+    htmlCode = htmlCode + `   
+            <div class="card">
+            <img src="${poke.img}"> 
+            <div class="contenido">
+              <h3>${poke.name}</h3>
+              <p>Número: ${poke.num} </p>
+                <p>Tipo: ${poke.type}</p>
+              <p>Resistencia: ${poke.resistant}</p>
+                <p>Debilidades: ${poke.weaknesses}</p>
+              </p>
+            </div>
+          </div>`
+  })
+
+
+
+  contenedor.innerHTML = htmlCode;  //insertar codigo html 
+}
+
+
+export const searchPokemon=(data)=>{
+  let input =document.getElementById('caja-texto');
+  let htmlCode= '';
+
+  data.forEach(poke=>{
+    if (poke.name===input.value){
+  htmlCode=
+  `   
+            <div class="card">
+            <img src="${poke.img}"> 
+            <div class="contenido">
+              <h3>${poke.name}</h3>
+              <p>Número: ${poke.num} </p>
+                <p>Tipo: ${poke.type}</p>
+              <p>Resistencia: ${poke.resistant}</p>
+                <p>Debilidades: ${poke.weaknesses}</p>
+              </p>
+            </div>
+          </div>`;
+     contenedor.innerHTML=htmlCode;
+    }
+
+  })
+
+}
+
--- src/main.js
+++ src/main.js
@@ -3,4 +3,22 @@ import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+import arrayPokemon from './data.js';
+import {templateCard,searchPokemon} from './data.js';
+
+let button=document.getElementById('btn-validar')
+
+window.addEventListener('load', showCard); //evento de carga
+function showCard(){                //funcion para el evento de carga
+    templateCard(arrayPokemon);
+
+}
+
+button.addEventListener('click',findPokemon);
+function findPokemon (){
+    searchPokemon(arrayPokemon);
+}
+
+
+
+
